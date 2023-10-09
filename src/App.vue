1d48293e4804b96a4ed336e433c68149ebad946d
MassimoAurelio <massimoaureliowork@gmail.com>
2023-10-09 15:14:05 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
+import Header from './components/AppHeader.vue'
 </script>
 
 <template>
   <header>
+    <Header />
   </header>
-
-  
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
