8c8a0d56a4a39da964f1998d15c1098c9b5bce85
MassimoAurelio <massimoaureliowork@gmail.com>
2023-11-03 10:06:19 +0300

    qwe

--- src/components/agents/AgentsPage.vue
+++ src/components/agents/AgentsPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch} from 'vue'
 import type { Agent, Role, Umeniya } from '../../types/interfaces'
 import { useRoute } from 'vue-router'
 import Popper from 'vue3-popper'
@@ -9,8 +9,8 @@ const route = useRoute()
 const agent = ref<Agent | null>(null)
 const role = ref<Role | null>(null)
 const skills = ref<Umeniya | null>(null)
+const selectedSkill = ref(null)
 const agentsClass = route.params.agentsClass
-
 const agentStore = useAgentStore()
 
 const fetchAgent = async () => {
@@ -23,6 +23,10 @@ const fetchAgent = async () => {
   agentStore.addAgent(data)
 }
 
+const showSkillInfo = (abi) => {
+  selectedSkill.value = abi
+}
+
 watch(
   () => route.params.agentsClass,
   () => {
@@ -30,6 +34,8 @@ watch(
   },
   { immediate: true }
 )
+
+
 </script>
 
 <template>
@@ -51,13 +57,18 @@ watch(
         <img class="img-agent" :src="agent?.fullPortrait" alt="" />
       </div>
       <nav v-if="skills" class="navigation-panel-skills">
-        <div class="main-container-skills" v-for="(abi, index) in skills" :key="index">
-          <div class="agent-skills">
-            {{ abi?.displayName }}
+        <div class="main-navigation" v-for="(abi, index) in skills" :key="index">
+          <div class="main-container-skills" @click="showSkillInfo(abi)">
+            <div class="agent-skills">
+              {{ abi?.displayName }}
+            </div>
+            <img class="skill-img" :src="abi?.displayIcon" alt="" />
           </div>
-          <img class="skill-img" :src="abi?.displayIcon" alt="" />
         </div>
       </nav>
+      <div v-if="selectedSkill" class="skill-description">
+        {{ selectedSkill.description }}
+      </div>
     </div>
   </section>
 </template>
@@ -69,8 +80,9 @@ watch(
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding-left: 15%;
-  padding-right: 15%;
+  padding-left: 25%;
+  padding-right: 25%;
+  overflow: auto;
 }
 
 .name-img-agent {
@@ -145,9 +157,18 @@ watch(
   font-weight: 700;
 }
 .main-container-skills {
-  min-width: 100px;
-
+  width: 100%;
+  cursor: pointer;
   border: 2px solid azure;
   padding: 5%;
+}
+
+.skill-description {
+  margin-top: 3%;
+  color: azure;
+}
+
+.main-container-skills:hover {
+  border-color: rgb(221, 120, 120);
 }
 </style>
